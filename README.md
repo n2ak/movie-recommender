e7f86d2dc82bed5483a14a2d5b668a358bc50141
n2ak <n2akajdaou@gmail.com>
2025-04-14 16:14:32 +0000

    modified readme

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Dataset
 
 [movielens](https://grouplens.org/datasets/movielens/)
+
+### Usage
+
+```bash
+docker compose up
+./start.ps1 # fill db for first time
+cd front/
+pnpm run dev
+```
--- start.ps1
+++ start.ps1
@@ -1,7 +1,10 @@
-cd front
+cd frontend
 # rm vlms -r -Force
-npx prisma migrate dev --name init
-python -m prisma migrate
+#npx prisma migrate dev --name init # migrate changes to db
+#python -m prisma generate # generate python files
+pnpm run db:generate
+pnpm run db:migrate
+
 cd ../back
-python filldb.py
+python scripts/filldb.py
 cd ..
