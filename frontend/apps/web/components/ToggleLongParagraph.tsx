f8425350c3fb70fe9636bfa07f06a77a4a42b101
n2ak <n2akajdaou@gmail.com>
2025-06-02 19:14:33 +0000

    remove paragraph toggle button if it is short

--- frontend/apps/web/components/ToggleLongParagraph.tsx
+++ frontend/apps/web/components/ToggleLongParagraph.tsx
@@ -1,20 +1,35 @@
 import { joinCN } from "@/lib/utils";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const ToggleLongParagraph = ({ text }: { text: string }) => {
   const [showP, setShowP] = useState(false);
-  //TODO case when text is short.
+  const [isOverflowing, setIsOverflowing] = useState(true);
+  const textRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    if (textRef.current)
+      setIsOverflowing(
+        textRef.current.scrollHeight > textRef.current.clientHeight
+      );
+  }, [text]);
+
   return (
     <div className="w-full">
-      <div className={joinCN("my-3", showP ? "" : "line-clamp-2")}>{text}</div>
-      <div className="text-right">
-        <span
-          className="text-xs underline cursor-pointer hover:font-bold hover:text-primary"
-          onClick={() => setShowP((s) => !s)}
-        >
-          {showP ? "show less" : "show more"}
-        </span>
+      <div
+        className={joinCN("my-3", showP ? "" : "line-clamp-2")}
+        ref={textRef}
+      >
+        {text}
       </div>
+      {isOverflowing && (
+        <div className="text-right">
+          <span
+            className="text-xs underline cursor-pointer hover:font-bold hover:text-primary"
+            onClick={() => setShowP((s) => !s)}
+          >
+            {showP ? "show less" : "show more"}
+          </span>
+        </div>
+      )}
     </div>
   );
 };
