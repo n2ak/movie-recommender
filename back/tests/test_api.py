d8fe0d98c9d00bf9d01e9b23616c6bffb25d7bd6
n2ak <n2akajdaou@gmail.com>
2025-09-13 09:25:28 +0000

    update api tests

--- back/tests/test_api.py
+++ back/tests/test_api.py
@@ -1,68 +1,57 @@
+from typing import Callable, TypeVar, Any
 from fastapi.testclient import TestClient
-from api import app, Response, SimilarMoviesRequest
+from api import app, RecomResponse, RecomRequest
+from movie_recommender.recommender import ModelType
+
+
 client = TestClient(app)
-MAX_RATING = 10
+
+MAX_RATING = 5
+
+T = TypeVar("T")
 
 
-def recomend_movies(data):
-    return client.post('/movies-recom', json=data)
+def recomend_movies(data: RecomRequest):
+    json = data.model_dump()
+    return client.post('/movies-recom', json=json)
 
 
-def recomend_similar_movies(data):
-    return client.post('/recom-similar-movies', json=data)
-
-
-def check_response(func, data):
+def check_response(func: Callable[[T], Any], data: T):
     resp_json = func(data)
     assert resp_json.status_code == 200, resp_json.status_code
-    result = Response(**resp_json.json())
+    result = RecomResponse(**resp_json.json())
     assert result.status_code == 200, result.error
-    assert result.userId == data["userId"]
+    assert result.userId == data.userId  # type: ignore
 
     for res in result.result:
-        assert 0 <= res["predicted_rating"] <= MAX_RATING
+        assert 0 <= res.predicted_rating <= MAX_RATING
+    return result
+
+
+MODELS: list[ModelType] = ["xgb_cpu", "xgb_cuda", "dlrm_cpu", "dlrm_cuda"]
 
 
 def test_api():
-    for relation in ["or", "and"]:
-        data = {
-            "userId": 1,
-            "genres": [],
-            "start": 0,
-            "count": None,
-            "relation": relation
-        }
+    for model in MODELS:
+        data = RecomRequest(
+            userId=1,
+            genres=[],
+            start=0,
+            count=None,
+            model=model,
+            temp=0,
+        )
         check_response(recomend_movies, data)
 
 
 def test_genres():
-    for relation in ["or", "and"]:
-        data = {
-            "userId": 1,
-            "genres": ["Action"],
-            "start": 0,
-            "count": None,
-            "relation": relation
-        }
+    for model in MODELS:
+        data = RecomRequest(
+            userId=1,
+            genres=["Action"],
+            start=0,
+            count=None,
+            model=model,
+            temp=0,
+        )
         check_response(recomend_movies, data)
-
-
-def test_api_no_movieIds():
-    data = {
-        "userId": 1,
-        "genres": ["Action", "Drama"],
-        "start": 0,
-        "count": None,
-        "relation": "or"
-    }
-    check_response(recomend_movies, data)
-
-
-def test_recomm_similar_movies():
-    data = {
-        "userId": 1,
-        "start": 0,
-        "count": None,
-        "movieIds": [1],
-    }
-    check_response(recomend_similar_movies, data)
