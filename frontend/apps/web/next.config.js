c8a20e20412adfe36b38ee3c71b053c9dedd53ff
n2ak <n2akajdaou@gmail.com>
2025-09-22 14:16:06 +0000

    comment

--- frontend/apps/web/next.config.js
+++ frontend/apps/web/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  output: "standalone",
+  output: "standalone", // for docker
   // eslint: {
   //   ignoreDuringBuilds: true,
   // },
