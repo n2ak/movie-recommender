8784daf5c56d9a27bce4fbfe814d0ae2adceb75a
n2ak <n2akajdaou@gmail.com>
2025-10-27 18:15:06 +0000

    use prisma migrate deploy

--- seed/script.sh
+++ seed/script.sh
@@ -5,8 +5,7 @@ echo "Generating Prisma client..."
 npx prisma@5.17.0 generate
 
 echo "Applying migrations..."
-# npx prisma@5.17.0 migrate deploy
-npx prisma@5.17.0 migrate dev --name init
+npx prisma@5.17.0 migrate deploy
 
 echo "Fetching data..."
 python get_ds.py
